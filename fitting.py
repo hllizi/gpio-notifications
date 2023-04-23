4c1dada7f6df352bae4ff78e8916ae022c883f45
David Lahm <david@klinger-lahm.de>
2023-04-23 11:56:50 +0200

    basic functionality available

--- fitting.py
+++ fitting.py
@@ -10,25 +10,59 @@ class SignalResponseComputer:
     def __init__(self, min_differerence):
         self.min_differerence = min_differerence
 
-    def computeSignalResponse(self, signal):
+    def __call__(self, signal):
      try: 
          difference = signal - self.last
          if difference >= datetime.timedelta(seconds = self.min_differerence):
             self.last = signal
             return signal
+         else: 
+            return None
      except TypeError as e:
          self.last = signal
          return signal
 
-src = SignalResponseComputer(5)
+class Control:
+    def __init__(self, eventReceiver, signalResponseComputer, messageSender):
+        self.signalResponseComputer = signalResponseComputer 
+        self.messageSender = messageSender
+        self.eventReceiver = eventReceiver
+        self.eventReceiver.setEventHandler(self.handleEvent)
+        self.eventReceiver.listen()
 
-current_time = datetime.datetime.now()
-print(src.computeSignalResponse(current_time))
-time.sleep(4)
-current_time = datetime.datetime.now()
-print("No time output now.")
-print(src.computeSignalResponse(current_time))
-time.sleep(6)
-current_time = datetime.datetime.now()
-print("Another time output now.")
-print(src.computeSignalResponse(current_time))
+    def computeMessage(self, eventValue):
+        return datetime.datetime.now()
+
+    def handleEvent(self, eventValue):
+        message = self.computeMessage(eventValue)
+        response = self.signalResponseComputer(message)
+        self.messageSender.send(response)
+
+class EventReceiver:
+    handler = None
+    eventSource = None
+
+    def __init__(self, eventSource):
+        self.eventSource = eventSource
+
+    def setEventHandler(self, handler):
+        self.handler = handler
+
+    def listen(self):
+        event = self.eventSource(True)
+        self.handler(event)
+        self.listen()
+
+class MessageSender:
+    def send(self, message):
+        print(message)
+
+control = Control(
+                    EventReceiver(lambda a: input("Type message: ")), 
+                    SignalResponseComputer(5), 
+                    MessageSender()
+                )
+
+
+
+
